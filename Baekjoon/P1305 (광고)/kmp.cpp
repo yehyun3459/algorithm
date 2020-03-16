92ca6da3f5cc92fc3d48edd50fae3a6bf2be9673
yehyun3459 <48521439+yehyun3459@users.noreply.github.com>
2020-03-16 17:39:56 +0900

    kmp fail함수를 이용해서 접두사 == 접미사길이를 구해준다

---
+++ Baekjoon/P1305 (광고)/kmp.cpp
@@ -0,0 +1,36 @@
+//KMP Fail 함수를 구해서,
+//접두사 == 접미사 부분의 길이를 빼주는 문제
+//문제 파악을 제대로 안했음.. ㅎㅎ
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#define MAX_SIZE 1000001
+#define MIN(a,b)(a<b?a:b)
+
+int L;
+char buffer[MAX_SIZE];
+int F[MAX_SIZE + 1];
+
+void make_fail()
+{
+	int i = 0;
+	int j = -1;
+	F[0] = -1;
+	while(i<L)
+	{
+		while (j > -1 && buffer[i] != buffer[j])
+			j = F[j];
+		i++, j++;
+		F[i] = j;
+		
+	}
+}
+
+int main()
+{
+	scanf("%d", &L);
+	scanf("%s", buffer);
+	buffer[L] = '\0';
+	make_fail();
+
+	printf("%d\n", L-F[L]);
+}
